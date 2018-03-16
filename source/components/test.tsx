b4d7d32452024c480e3de1ebceb4210933bb3ae3
Chase Moskal <chasemoskal@gmail.com>
2018-03-16 12:16:00 -0700

    introduce preact

---
+++ source/components/test.tsx
@@ -0,0 +1,12 @@
+
+import {h, Component} from "preact"
+
+export default class Test extends Component<any, any> {
+	render() {
+		return (
+			<div className="test">
+				Test
+			</div>
+		)
+	}
+}
---
+++ source/demo-script.tsx
@@ -0,0 +1,5 @@
+
+import {h, render} from "preact"
+import Test from "./components/test"
+
+render(<Test/>, document.querySelector(".lz"))
