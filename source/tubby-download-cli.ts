1f0124368a29befe64bd37b2dbd4e5dafec4cf4a
Chase Moskal <chasemoskal@gmail.com>
2019-05-02 15:29:25 -0700

    modernize distribution, and restore node download

--- source/tubby-download-cli.ts
+++ source/tubby-download-cli.ts
@@ -1,19 +1,23 @@
+#!/usr/bin/env node
 
+import * as fetch from "node-fetch"
 import * as commander from "commander"
-import {downloadCannedVideos} from "./youtube/download-canned-videos.js"
+import {dieOnError} from "die-on-error"
+
+import {getPlaylistVideos} from "./youtube/get-playlist-videos.js"
 
 commander
-	.option("-a, --api-key", "google api key")
-	.option("-p, --playlist-id", "youtube playlist id")
+	.option("-a, --api-key <api-key-string>", "google api key")
+	.option("-p, --playlist-id <playlist-id-string>", "youtube playlist id")
 	.parse(process.argv)
 
 async function main() {
 	const {apiKey, playlistId} = commander
 	if (!apiKey) throw new Error("--api-key required")
 	if (!playlistId) throw new Error("--playlist-id required")
-
-	const videos = await downloadCannedVideos({apiKey, playlistId})
+	const videos = await getPlaylistVideos({fetch, apiKey, playlistId})
 	process.stdout.write(JSON.stringify(videos))
 }
 
+dieOnError()
 main()
