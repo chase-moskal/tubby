df154224fdef606949e1abd5c48134d3eb2a4324
Chase Moskal <chasemoskal@gmail.com>
2019-05-21 15:32:13 -0700

    improve loading/error svg customizability

--- source/components/tubby-youtube-explorer.ts
+++ source/components/tubby-youtube-explorer.ts
@@ -2,7 +2,7 @@
 import {TubbyError} from "../tubby-error.js"
 import {Video, ThumbSize} from "../interfaces.js"
 import {getUploads} from "../youtube/get-uploads.js"
-import {Component, html, prop} from "../toolbox/component.js"
+import {Component, html, svg, prop} from "../toolbox/component.js"
 import {getPlaylistVideos} from "../youtube/get-playlist-videos.js"
 
 import {TubbySearch} from "./tubby-search.js"
@@ -93,9 +93,22 @@ export class TubbyYoutubeExplorer extends Component {
 					text-align: center;
 				}
 
+				.pending > svg, .error > svg {
+					display: inline-block;
+					width: 32px;
+					height: 32px;
+					vertical-align: middle;
+					margin-right: 0.5em;
+				}
+
 				.pending {
 					background: var(--tubby-pending-bg, rgba(0,0,0, 0.2));
 					color: var(--tubby-pending-color, white);
+				}
+
+				.pending > svg {
+					animation: rotation 8s infinite linear;
+					fill: var(--tubby-pending-color, white);
 				}
 
 				.error {
@@ -103,24 +116,8 @@ export class TubbyYoutubeExplorer extends Component {
 					color: var(--tubby-error-color, yellow);
 				}
 
-				.pending::before, .error::before {
-					content: "";
-					display: inline-block;
-					background-size: cover;
-					background-position: center center;
-					width: 32px;
-					height: 32px;
-					vertical-align: middle;
-					margin-right: 0.5em;
-				}
-
-				.pending::before {
-					background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="12" height="16" viewBox="0 0 12 16"><path fill="white" fill-rule="evenodd" d="M10.24 7.4a4.15 4.15 0 0 1-1.2 3.6 4.346 4.346 0 0 1-5.41.54L4.8 10.4.5 9.8l.6 4.2 1.31-1.26c2.36 1.74 5.7 1.57 7.84-.54a5.876 5.876 0 0 0 1.74-4.46l-1.75-.34zM2.96 5a4.346 4.346 0 0 1 5.41-.54L7.2 5.6l4.3.6-.6-4.2-1.31 1.26c-2.36-1.74-5.7-1.57-7.85.54C.5 5.03-.06 6.65.01 8.26l1.75.35A4.17 4.17 0 0 1 2.96 5z"/></svg>');
-					animation: rotation 8s infinite linear;
-				}
-
-				.error::before {
-					background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill="white" fill-rule="evenodd" d="M8.893 1.5c-.183-.31-.52-.5-.887-.5s-.703.19-.886.5L.138 13.499a.98.98 0 0 0 0 1.001c.193.31.53.501.886.501h13.964c.367 0 .704-.19.877-.5a1.03 1.03 0 0 0 .01-1.002L8.893 1.5zm.133 11.497H6.987v-2.003h2.039v2.003zm0-3.004H6.987V5.987h2.039v4.006z"/></svg>');
+				.error > svg {
+					fill: var(--tubby-error-color, yellow);
 				}
 
 				.info {
@@ -190,11 +187,17 @@ export class TubbyYoutubeExplorer extends Component {
 		`
 
 		const pending = html`
-			<div class="pending">loading videos...</div>
+			<div class="pending">
+				${svg`<svg xmlns="http://www.w3.org/2000/svg" width="12" height="16" viewBox="0 0 12 16"><path fill="white" fill-rule="evenodd" d="M10.24 7.4a4.15 4.15 0 0 1-1.2 3.6 4.346 4.346 0 0 1-5.41.54L4.8 10.4.5 9.8l.6 4.2 1.31-1.26c2.36 1.74 5.7 1.57 7.84-.54a5.876 5.876 0 0 0 1.74-4.46l-1.75-.34zM2.96 5a4.346 4.346 0 0 1 5.41-.54L7.2 5.6l4.3.6-.6-4.2-1.31 1.26c-2.36-1.74-5.7-1.57-7.85.54C.5 5.03-.06 6.65.01 8.26l1.75.35A4.17 4.17 0 0 1 2.96 5z"/></svg>`}
+				loading videos...
+			</div>
 		`
 
 		const error = html`
-			<div class="error">error loading videos</div>
+			<div class="error">
+				${svg`<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M8.893 1.5c-.183-.31-.52-.5-.887-.5s-.703.19-.886.5L.138 13.499a.98.98 0 0 0 0 1.001c.193.31.53.501.886.501h13.964c.367 0 .704-.19.877-.5a1.03 1.03 0 0 0 .01-1.002L8.893 1.5zm.133 11.497H6.987v-2.003h2.039v2.003zm0-3.004H6.987V5.987h2.039v4.006z"/></svg>`}
+				error loading 
+			</div>
 		`
 
 		const ready = html`
